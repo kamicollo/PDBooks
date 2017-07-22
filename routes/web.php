c3057155005bf08228b7a5af30c872df342b01e8
Aurimas R <mail@aurimas.eu>
2017-07-22 23:14:28 +0200

    Working version of Goodreads - need cleanup.

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,33 @@ Route::get('book/{id?}', function($id = 1) {
 Route::get('book/{id}/chapter/{number}', function($id, $number) {
     return view('chapter', ['bodyclass' => 'read', 'book' => App\Book::find($id), 'chapter' => App\Book::find($id)->chapters()->find($number)]);
 });
+
+Route::get('book/{id}/goodreads', function($id) {
+    $book = App\Book::find($id);
+    try {
+        $client = new GuzzleHttp\Client(['base_uri' => 'https://www.goodreads.com/book/isbn/']);
+        $response = $client->request('GET', $book->isbn, 
+            ['query' => 
+                [
+                    'key'    => env('GOODREADS_KEY'),
+                    'format' => 'xml'
+                ]
+            ]
+        );
+        
+        $xml = \simplexml_load_string($response->getBody(), null, LIBXML_NOCDATA);
+        $number_of_ratings = (int) $xml->xpath("book/work/ratings_count/text()")[0];
+        $sum_of_ratings = (int) $xml->xpath("book/work/ratings_sum/text()")[0];
+        $url = $xml->xpath("book/url")[0];
+        $average_rating = $sum_of_ratings / $number_of_ratings;
+        $book->goodreads_avg_rating = $average_rating;
+        $book->goodread__countof_ratings = $number_of_ratings;
+        $book->link_to_goodreads = $url;
+        $book->save();
+    } catch (\ErrorException $error) {
+        //Pokemons!
+    }
+    return $book;
+    
+
+});
