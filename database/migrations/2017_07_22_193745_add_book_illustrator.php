a3fa15ad9988fc0345893d4ebf473faa5057f23d
Aurimas R <mail@aurimas.eu>
2017-07-22 21:42:22 +0200

    Adding translator field in the database.

---
+++ database/migrations/2017_07_22_193745_add_book_illustrator.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBookIllustrator extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('translator', 200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('translator');
+        });
+    }
+}
