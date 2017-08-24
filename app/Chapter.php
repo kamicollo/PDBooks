4ebf57a4fee5b89ff446fd7b6880b1c56434a42a
Ernesta Orlovaitė <ernesta@ernes7a.lt>
2017-08-24 23:12:07 +0200

    Added chapter word counts.

--- app/Chapter.php
+++ app/Chapter.php
@@ -5,64 +5,66 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
+class Chapter extends Model {
+	public $timestamps = false;
 
-class Chapter extends Model
-{
-    public $timestamps = false;
+	public function book() {
+		return $this->belongsTo('App\Book');
+	}
 
-    public function book()
-    {
-        return $this->belongsTo('App\Book');
-    }
+	public function isNext() {
+		return $this->book()->first()->chapters()->max('order') != $this->order;
+	}
 
-    public function isNext() {
-        return $this->book()->first()->chapters()->max('order') != $this->order;
-    }
+	public function isPrevious() {
+		return $this->book()->first()->chapters()->min('order') != $this->order;
+	}
 
-    public function isPrevious() {
-        return $this->book()->first()->chapters()->min('order') != $this->order;
-    }
-	
 	public function getNext() {
 		return $this->book()->first()
 			->chapters()->where('order', '>', $this->order)
 			->orderBy('order', 'asc')->first()->order;
 	}
-	
+
 	public function getPrevious() {
 		return $this->book()->first()
 			->chapters()->where('order', '<', $this->order)
 			->orderBy('order', 'desc')->first()->order;
 	}
-	
-	public function getReadingTime() {
+
+	public function getWordCount() {
 		$text = strip_tags($this->content);
-		return str_word_count($text) / 250;
+		return str_word_count($text);
 	}
-	
+
+	public function getReadingTime() {
+		return $this->getWordCount() / 250;
+	}
+
 	use \web_helpers;
-	
+
 	public function web_page_title($site_name = FALSE) {
 		return $this->page_title(
 			$site_name,
 			[
-				$this->title, 
+				$this->title,
 				$this->book()->first()->title . ' by ' . $this->book()->first()->author
 			]
 		);
 	}
-	
+
 	public function web_url() {
 		return route('chapter', ['id' => $this->book()->first()->getRouteKey(), 'order' => $this->order]);
 	}
-	
+
 	public function web_image() {
 		return $this->book()->first()->background_image;
 	}
-	
+
 	public function web_description() {
 		$descr = str_replace(["\n", "\t", "\r"], " ", trim(strip_tags($this->content)));
 		$descr = preg_replace('#\s+#', " ", $descr);
+
 		//Regex from: https://stackoverflow.com/questions/1231959/180-first-characters-of-a-string-ending-with-a-word
 		$m = preg_match("/^.{1,200}\b(?<!\s)/", $descr, $matches);
 		if ($m) {
@@ -71,5 +73,4 @@ class Chapter extends Model
 			return '';
 		}
 	}
-	
 }
--- resources/views/chapter.blade.php
+++ resources/views/chapter.blade.php
@@ -13,7 +13,7 @@
 		<h1>{{$chapter->title}}</h1>
 		<div class="readability">
 			<span class="label label-default" data-toggle="tooltip" data-placement="bottom" title="Based on an average reading speed of 250 words per minute.">{{ceil($chapter->getReadingTime())}} min read</span>
-			<span class="label label-default">2,136 words</span>
+			<span class="label label-default">{{number_format($chapter->getWordCount())}} words</span>
 			<span class="label label-default" data-toggle="tooltip" data-placement="bottom" title="Easily understood by an average 7th or 8th-grade student (Dale–Chall readability formula).">Readability score 6.6</span>
 		</div>
 
