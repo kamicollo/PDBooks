eae4323858a35fd1c2f9af25013aed1c2b053d7f
Ernesta Orlovaitė <ernesta@ernes7a.lt>
2017-08-24 23:01:18 +0200

    Added full Alice in Wonderland text.

--- database/seeds/BookSeeder.php
+++ database/seeds/BookSeeder.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Database\Seeder;
 
-class BookSeeder extends Seeder
-{
+class BookSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         DB::table('books')->insert([
             'title' => "Alice's Adventures in Wonderland",
             'author' => "Lewis Carroll",
@@ -25,9 +23,9 @@ class BookSeeder extends Seeder
             'background_image' => "images/bg.svg",
             'cover_image' => "images/cover.jpg",
             'description' => "Alice's Adventures in Wonderland (commonly shortened to Alice in Wonderland) is an 1865 fantasy novel written by English mathematician Charles Lutwidge Dodgson under the pseudonym Lewis Carroll. It tells of a girl named Alice falling through a rabbit hole into a fantasy world populated by peculiar, anthropomorphic creatures. The tale plays with logic, giving the story lasting popularity with adults as well as with children.",
-			'descr_quote' => '<p>“No wise fish would go anywhere without a porpoise.”</p>',
-			'descr_source' => '<a target="_blank" href="https://en.wikipedia.org/wiki/Alice%27s_Adventures_in_Wonderland">Wikipedia</a>',
-			'url_slug' => "alice-in-wonderland"
+            'descr_quote' => '<p>“No wise fish would go anywhere without a porpoise.”</p>',
+            'descr_source' => '<a target="_blank" href="https://en.wikipedia.org/wiki/Alice%27s_Adventures_in_Wonderland">Wikipedia</a>',
+            'url_slug' => "alice-in-wonderland"
         ]);
     }
 }
